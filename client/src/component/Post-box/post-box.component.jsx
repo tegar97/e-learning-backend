c59572ba1b035cdafcca7dbcc98d1f7b5eeee8df
Muhammad Tegar Akmal <eategar@gmail.com>
2021-02-17 11:25:43 +0700

    update link image

--- client/src/component/Post-box/post-box.component.jsx
+++ client/src/component/Post-box/post-box.component.jsx
@@ -58,12 +58,12 @@ function PostBox({match,data : {id,content,content_title,created_by,file,created
                     </PostTaskContainer> 
                     <PostImageContainer>
                     {imageType.includes(file.split('.').pop().toLowerCase()) ?
-                     <img src={ `${process.env.api}images/${file}` } alt="image" width="100%" height="400" style={{objectFit: 'cover'}}/>
+                     <img src={ `https://e-learning-backend.tegar.me/images/${file}` } alt="image" width="100%" height="400" style={{objectFit: 'cover'}}/>
                     : file ?
                      <PostFileContainer>
                         <FileCopyIcon/>
                         <p style={{marginRight: 'auto'}}>{file}</p>
-                        <a download href={`${process.env.api}images/${file}`}>download</a>
+                        <a download href={`https://e-learning-backend.tegar.me/images/${file}`}>download</a>
                      </PostFileContainer>   
                     :
                     ''
